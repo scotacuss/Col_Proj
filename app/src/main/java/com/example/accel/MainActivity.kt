635d995ed0b94295eecc5b2fb37353caaed3dc4a
scotacuss <scotacuss@gmail.com>
2024-02-03 20:31:44 +0000

    stuck on collision

--- app/src/main/java/com/example/accel/MainActivity.kt
+++ app/src/main/java/com/example/accel/MainActivity.kt
@@ -7,6 +7,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.media.Image
 import android.os.Bundle
 import android.view.View
 import android.widget.GridLayout
@@ -16,8 +17,7 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-
-
+import kotlin.math.absoluteValue
 
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
@@ -27,20 +27,50 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var details_1: TextView
     private lateinit var details_2: TextView
     private lateinit var meas: View
-    private lateinit var mv:RelativeLayout
+    private lateinit var mv: RelativeLayout
 
+    lateinit var obs1: ImageView
 
     private lateinit var ll_lay: LinearLayout
 
-    fun createObs(x: Float, y:Float, ht:Int, wd:Int): ImageView {
+
+    fun createObs(x: Float, y: Float, ht: Int, wd: Int, shape: String = "square"): ImageView {
         var obs = ImageView(this)
-        obs.layoutParams = LinearLayout.LayoutParams(wd,ht)
+        if (shape == "circle") {
+            obs.setImageResource(R.drawable.circ_obs)
+        } else {
+            obs.setBackgroundColor(Color.parseColor("#000000"))
+        }
+        obs.layoutParams = LinearLayout.LayoutParams(wd, ht)
         obs.x = x
         obs.y = y
-        obs.setBackgroundColor(Color.parseColor("#FFFFFFFF"))
+
         mv.addView(obs)
         return obs
     }
+
+
+    fun colDetec(projectile: ImageView, obs: ImageView, obs_shape: String = "square"){
+        val pro_x = projectile.x + (projectile.width / 2)
+        val pro_y = projectile.y + (projectile.height / 2)
+        val obs_R = obs.x + obs.width
+        val obs_B = obs.y + obs.height
+
+
+            if (pro_y > obs.y && pro_y < obs_B && (obs.x - pro_x) < projectile.width/2){
+                projectile.x -= 1
+                 }
+            else if (pro_y > obs.y && pro_y < obs_B && (pro_x - obs_R) < projectile.width/2){
+                projectile.x += 1
+            }
+            else if (pro_x > obs.x && pro_x < obs_R && (obs.y - pro_y) < projectile.height/2){
+                projectile.y -= 1
+            }
+            else if (pro_x > obs.x && pro_x < obs_R && (pro_y - obs_B) < projectile.width/2){
+                projectile.y += 1
+            }
+        }
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,20 +88,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
 
         // Obstacles
-
-
-        var obs1 = createObs(300F,300F,200,200)
-        var obs2 = createObs(600F,600F,100,100)
+        obs1 = createObs(500F, 900F, 100, 700)
     }
 
 
-
-
-    private fun setUpSensorStuff(){
+    private fun setUpSensorStuff() {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
 
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
-            sensorManager.registerListener(this,it,SensorManager.SENSOR_DELAY_FASTEST, SensorManager.SENSOR_DELAY_FASTEST)
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
         }
     }
 
@@ -84,7 +114,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private val terminal_velo = 10
     private val dampner = 0.8F // coefficient of restitution
 
-    fun handleBoundaryCollision(coord: Float, minBound: Float, maxBound: Float, velocity: Float): Pair<Float, Float> {
+    fun screenBoundaryCollision(
+        coord: Float,
+        minBound: Float,
+        maxBound: Float,
+        velocity: Float
+
+    ): Pair<Float, Float> {
         val newCoord = when {
             coord > maxBound -> maxBound
             coord < minBound -> minBound
@@ -104,18 +140,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             val rightBounds = (meas.right - ball.width).toFloat()
             val bottomBounds = (meas.bottom - ball.height).toFloat()
 
-            xAccel = sides/100
-            yAccel = upDown/100
+
+            xAccel = sides / 100
+            yAccel = upDown / 100
 
             xVelo += xAccel
             yVelo += yAccel
 
-            if (xVelo > terminal_velo || xVelo < -terminal_velo){
-                xVelo = (terminal_velo-0.5).toFloat()
+
+            if (xVelo > terminal_velo || xVelo < -terminal_velo) {
+                xVelo = (terminal_velo - 0.5).toFloat()
             }
 
-            if (yVelo > terminal_velo || yVelo < -terminal_velo){
-                yVelo = (terminal_velo-0.5).toFloat()
+            if (yVelo > terminal_velo || yVelo < -terminal_velo) {
+                yVelo = (terminal_velo - 0.5).toFloat()
             }
 
 
@@ -124,26 +162,24 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 ball.x -= xVelo.toFloat()
                 ball.y += yVelo.toFloat()
 
-//                if (obs.x < ball.x && ball.x < obs.x+obs.width && obs.y < ball.y && ball.y > obs.y+obs.height){
-//                    ball.x = 250F
-//                    ball.y = 250F
-//                }
 
                 // Screen Boundary Collision Logic
-                val (newX, newXVelo) = handleBoundaryCollision(ball.x,0F, rightBounds, xVelo)
+                val (newX, newXVelo) = screenBoundaryCollision(ball.x, 0F, rightBounds, xVelo)
                 ball.x = newX
                 xVelo = newXVelo
 
-                val (newY, newYVelo) = handleBoundaryCollision(ball.y,0F, bottomBounds, yVelo)
+                val (newY, newYVelo) = screenBoundaryCollision(ball.y, 0F, bottomBounds, yVelo)
                 ball.y = newY
                 yVelo = newYVelo
+
+                colDetec(ball,obs1)
             }
 
 
-            details_1.text = "x Velocity ${meas.width/2}\ny Velocity ${meas.height/2}\n"
+            details_1.text = "x Velocity ${xAccel}\n"
             details_2.text = "ball x ${ball.x}\nball y ${ball.y}\n"
-            }
         }
+    }
 
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
